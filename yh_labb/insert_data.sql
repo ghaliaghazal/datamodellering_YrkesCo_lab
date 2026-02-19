04dd8783d72162064b0f54db425ba4c0c9632bbd
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2026-02-20 00:21:33 +0100

    Create insert_data.sql and insert fake data for Campus, Program and Course

---
+++ yh_labb/insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Campus" (campus_id, location) VALUES
+(1, 'Stockholm'),
+(2, 'Göteborg');
+
+INSERT INTO "Program" (program_id, program_name, description) VALUES
+(1, 'Data Engineer' , 'YH program inom data'), 
+(2, 'Systemutvecklare' , 'YH program inom systemutveckling');
+
+INSERT INTO "Course" (course_id, course_name, course_code, credits, description)
+VALUES
+(1, 'Datamodellering', 'DM25', 20, 'Grundläggande datamodellering'),
+(2, 'Databaser', 'DB25', 30, 'SQL och databashantering');
+
