742a61dc6d747e3c600627aa12a08f1b317628f1
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2026-02-20 07:19:18 +0100

    Added test queries for Class and Programleader with join between tables

--- yh_labb/test_queries.sql
+++ yh_labb/test_queries.sql
@@ -1,9 +1,7 @@
-SELECT "Class" .class_id, 
-"ProgramLeader" .first_name, "ProgramLeader" .last_name
-FROM "Class"
-JOIN "ProgramLeader" ON "Class" .leader_id = "ProgramLeader" .leader_id
-WHERE "Class" .class_id = 1;
-   
-
 SELECT * FROM "Class";
-SELECT * FROM "ProgramLeader";
+SELECT * FROM "ProgramLeader"; 
+
+SELECT "Class" .class_id, 
+"ProgramLeader".first_name, "ProgramLeader".last_name
+FROM "Class"
+JOIN "ProgramLeader" ON "Class".leader_id = "ProgramLeader".leader_id;
